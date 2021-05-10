d75eb7c718526fe95e206c2632109a0d9f68e507
victordiaspalmeira <victordiaspalmeira@gmail.com>
2021-05-10 16:47:34 -0300

    data processing e treinamento

---
+++ data_processing.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+from matplotlib import pyplot as plt
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import BayesianRidge, LogisticRegression, LassoLars
+
+def clear_dataset(df):
+    df.index.names = [None]
+    df.set_index('timestamp', inplace=True)
+    df.index = pd.to_datetime(df.index)
+    df = df[~df.index.duplicated()]
+
+    if 'SSD' in df.columns:
+        df.drop('SSD', inplace=True, axis=1)
+    if 'COP' in df.columns:
+        df.drop('COP', inplace=True, axis=1)
+    if 'Pot_kw' in df.columns:
+        df.drop('Pot_kw', inplace=True, axis=1)
+    if 'Tsh' in df.columns:
+        df.drop('Tsh', inplace=True, axis=1)
+    if 'Tsc' in df.columns:
+        df.drop('Tsc', inplace=True, axis=1)
+    if 'dev_id' in df.columns:
+        df.drop('dev_id', inplace=True, axis=1)
+
+    return df
+
+def prepare_dataset(df):
+    sampling = '10Min'
+    df = df.resample(sampling).max()
+    start_date = df.index[0]
+    end_date = df.index[-1]
+
+    cols = df.columns
+
+    idx = pd.date_range(start=pd.Timestamp(start_date), end=pd.Timestamp(end_date), freq='10Min')
+    df = df.reindex(idx)
+
+    imputer = IterativeImputer(LogisticRegression())
+    impute_data = pd.DataFrame(imputer.fit_transform(df))
+    impute_data.columns = cols
+    impute_data.index = df.index
+
+    return impute_data
+
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df=None, val_df=None, test_df=None,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+    
+  def split_window(self, features):
+    inputs = features[:, self.input_slice, :]
+    labels = features[:, self.labels_slice, :]
+    if self.label_columns is not None:
+      labels = tf.stack(
+          [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+          axis=-1)
+
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+    inputs.set_shape([None, self.input_width, None])
+    labels.set_shape([None, self.label_width, None])
+
+    return inputs, labels
+
+def split_window(self, features):
+    inputs = features[:, self.input_slice, :]
+    labels = features[:, self.labels_slice, :]
+    if self.label_columns is not None:
+      labels = tf.stack(
+          [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+          axis=-1)
+
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+    inputs.set_shape([None, self.input_width, None])
+    labels.set_shape([None, self.label_width, None])
+
+    return inputs, labels
+
+WindowGenerator.split_window = split_window
+
+def plot(self, model=None, plot_col='Temperature', max_subplots=3):
+  inputs, labels = self.example
+  plt.figure(figsize=(12, 8))
+  plot_col_index = self.column_indices[plot_col]
+  max_n = min(max_subplots, len(inputs))
+  for n in range(max_n):
+    plt.subplot(max_n, 1, n+1)
+    plt.ylabel(f'{plot_col} [normed]')
+    plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+             label='Inputs', marker='.', zorder=-10)
+
+    if self.label_columns:
+      label_col_index = self.label_columns_indices.get(plot_col, None)
+    else:
+      label_col_index = plot_col_index
+
+    if label_col_index is None:
+      continue
+
+    plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                edgecolors='k', label='Labels', c='#2ca02c', s=64)
+    if model is not None:
+      predictions = model(inputs)
+      plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                  marker='X', edgecolors='k', label='Predictions',
+                  c='#ff7f0e', s=64)
+
+    if n == 0:
+      plt.legend()
+
+  plt.xlabel('Time [h]')
+  plt.savefig('plot.png')
+
+WindowGenerator.plot = plot
+
+def make_dataset(self, data):
+  data = np.array(data, dtype=np.float32)
+  ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+      data=data,
+      targets=None,
+      sequence_length=self.total_window_size,
+      sequence_stride=1,
+      shuffle=True,
+      batch_size=32,)
+
+  ds = ds.map(self.split_window)
+
+  return ds
+
+WindowGenerator.make_dataset = make_dataset
+
+@property
+def train(self):
+  return self.make_dataset(self.train_df)
+
+@property
+def val(self):
+  return self.make_dataset(self.val_df)
+
+@property
+def test(self):
+  return self.make_dataset(self.test_df)
+
+@property
+def example(self):
+  """Get and cache an example batch of `inputs, labels` for plotting."""
+  result = getattr(self, '_example', None)
+  if result is None:
+    # No example batch was found, so get one from the `.train` dataset
+    result = next(iter(self.train))
+    # And cache it for next time
+    self._example = result
+  return result
+
+WindowGenerator.train = train
+WindowGenerator.val = val
+WindowGenerator.test = test
+WindowGenerator.example = example
--- dut_model.py
+++ dut_model.py
@@ -9,6 +9,7 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.linear_model import BayesianRidge, LogisticRegression, LassoLars
+from data_processing import *
 
 # constantes
 seed = 2021
@@ -16,10 +17,12 @@ np.random.seed(seed)
 tf.random.set_seed(seed)
 N_TRAIN = int(1e4)
 BATCH_SIZE = 128
+MAX_EPOCHS = 3000
 STEPS_PER_EPOCH = N_TRAIN//BATCH_SIZE
 data_steps = 4*6
+OUT_STEPS = data_steps
 np.seterr(divide='ignore')
-
+patience = 350
 
 class DutModel():
     def __init__(self, dev_id : str):
@@ -37,6 +40,41 @@ class DutModel():
         pass
 
     def train(self, training_data: pd.DataFrame) -> np.double:
+        """
+        Normalização e limpeza dos dados
+        """
+
+        #Split datasets
+        n = len(df)
+        train_df = training_data[0:int(n*0.7)]
+        val_df = training_data[int(n*0.7):int(n*0.9)]
+        test_df = training_data[int(n*0.9):]
+
+        num_features = training_data.shape[1]
+
+        #Standarize
+        train_mean = train_df.mean()
+        train_std = train_df.std()
+
+        train_df = (train_df - train_mean)/train_std
+        val_df = (val_df - train_mean)/train_std
+        test_df = (test_df - train_mean)/train_std
+
+        """
+        Criação de janela de dados
+        """
+        window = WindowGenerator(input_width=data_steps,
+                                    label_width=data_steps,
+                                    shift=data_steps,
+                                    train_df=train_df, val_df=val_df, test_df=test_df
+                                    )       
+
+        window.plot()
+
+        """
+        Modelo
+        """
+        #Modelo
         self.model = tf.keras.Sequential([
             tf.keras.layers.LSTM(64, input_shape=(
             train_df.shape[0], train_df.shape[1]), return_sequences=True),
@@ -48,10 +86,42 @@ class DutModel():
             tf.keras.layers.Reshape([OUT_STEPS, num_features])
         ])
 
-        pass
+        lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+            0.01,
+            decay_steps=STEPS_PER_EPOCH*1000,
+            decay_rate=1,
+            staircase=False
+        )        
+
+        early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                            patience=patience,
+                                                            mode='auto',
+                                                            restore_best_weights=True)
+
+        self.model.compile(loss=tf.losses.MeanSquaredError(),
+                            optimizer=tf.optimizers.Adam(lr_schedule),
+                            metrics=[tf.metrics.MeanAbsoluteError()])        
+
+        history = self.model.fit(window.train, epochs=MAX_EPOCHS,
+                            validation_data=window.val,
+                            callbacks=[early_stopping])
+
+        return history
 
     def save_dataset(self):
         pass
 
     def load_dataset(self):
         pass
+
+if __name__ == '__main__':
+    dev_id = 'DUT209201107'
+    df = pd.read_csv('DUT209201107_20210301T000000_20210325T000000.csv')
+
+    df = clear_dataset(df)
+    df = prepare_dataset(df)
+    df = df['2021-03-03':'2021-03-12']
+
+    dutModel = DutModel(dev_id=dev_id)
+    history = dutModel.train(training_data=df)
+
